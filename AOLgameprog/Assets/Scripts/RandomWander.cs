d95e15aeb9669a8662ac0b4384597bdf9a9f6aea
Krisenna <zhengxin0505@gmail.com>
2025-12-15 15:20:38 +0700

    test

--- AOLgameprog/Assets/Scripts/RandomWander.cs
+++ AOLgameprog/Assets/Scripts/RandomWander.cs
@@ -11,10 +11,18 @@ public class RandomWander : MonoBehaviour
     private Vector2 targetPosition;
     private bool isMoving = false;
     private Rigidbody2D rb;
+    private Animator animator;
+
+    [Range(0f, 1f)]
+    public float idleChance = Random.Range(0.2f, 0.5f);   // 30% chance diem
+
+    public float idleTimeMin = 2f;
+    public float idleTimeMax = 5f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         StartCoroutine(WanderRoutine());
     }
 
@@ -38,15 +46,31 @@ public class RandomWander : MonoBehaviour
 
         yield return new WaitForSeconds(moveInterval);
 
-        // Pilih arah random dari 4 arah
-        Vector2 randomDir = directions[Random.Range(0, directions.Length)] * moveRadius;
-        targetPosition = (Vector2)transform.position + randomDir;
+        // Random chance buat idle
+        if (Random.value < idleChance)
+        {
+            isMoving = false;
+            animator.SetFloat("Speed", 0f);
 
-        // Cek collision
-        if (!Physics2D.OverlapCircle(targetPosition, 0.2f, obstacleMask))
+            float idleTime = Random.Range(idleTimeMin, idleTimeMax);
+            yield return new WaitForSeconds(idleTime);
+            continue; // skip movement, balik ke loop
+        }
+
+            // Pilih arah random dari 4 arah
+            Vector2 randomDir = directions[Random.Range(0, directions.Length)] * moveRadius;
+        targetPosition = (Vector2)transform.position + randomDir;
+        if (randomDir.x > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (randomDir.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+            // Cek collision
+            if (!Physics2D.OverlapCircle(targetPosition, 0.2f, obstacleMask))
         {
             isMoving = true;
-            float moveTime = 0.5f;
+            animator.SetFloat("Speed", isMoving ? 1f : 0f);
+                float moveTime = 0.5f;
             float elapsed = 0f;
             Vector2 startPos = transform.position;
 
@@ -64,6 +88,7 @@ public class RandomWander : MonoBehaviour
                 rb.MovePosition(targetPosition);
 
             isMoving = false;
+            animator.SetFloat("Speed", 0f);
         }
         else
         {
